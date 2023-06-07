aef3a06ee7174d51e0037c208c8bbbe7ec977580
Hari Kandregula <kandregulahari0197@gmail.com>
2023-06-07 23:55:08 +0530

    now create posts working and need to add custom user mmdel

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Post
-from .forms import userCreationForm
+from blog.forms import userCreationForm
 
 # Create your views here.
 
 def createPost(request):
-    #ts = Post.objects.all()
+    posts = Post.objects.all()
     #return render(request, 'createID.html', {'posts': posts})
     return render(request, 'createPost.html')
 
@@ -17,25 +17,31 @@ def post(request, pk):
     return render(request, 'post.html', {'posts': posts})
 
 def home(request):
-    '''
     posts = Post.objects.all()
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST.get('name') == 'new_post':
         post = Post()
         post.title = request.POST.get('postheading')
         post.body = request.POST.get('postcontent')
-        return render(request, 'home.html', {'posts': posts})
-        post.save()'''
-    posts = Post.objects.all()
+        post.save()
+    
+    if request.method =='POST' and request.POST.get('name') == 'new_user':
+        user = Users.objects.all()
+        '''
+        username1 = request.POST['username']
+        password1 = request.POST['password']
+        new_user = Users.objects.create(username = username1, password = password1)'''
+        new_user = Users()
+        new_user.username  = request.POST['username']
+        new_user.password = request.POST['password']
+        new_user.save()
     return render(request, 'home.html', {'posts': posts})
 
 def createUser(request):
-    if request.method =='POST':
-        #user = Users()
-        '''
+    if request.method =='POST1':
+        user = Users().objects.all()
         username1 = request.POST['username']
         password1 = request.POST['password']
         new_user = Users.objects.create(username = username1, password = password1)
         new_user.save()
-    return render(request, 'home.html')'''
-        form = userCreationForm(request.POST)
+    return render(request, 'home.html')
         
